7d1031cb9030dde5342cc349f761def3a2b76084
Andrea Sorbini <asorbini@rti.com>
2024-04-21 00:53:21 -0700

    Test pyconfig

--- pyconfig/__init__.py
+++ pyconfig/__init__.py
@@ -13,9 +13,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 ###############################################################################
-from .pyconfig import configure, summarize
+from .pyconfig import configure, configuration, summarize
 
 __all__ = [
   "configure",
+  "configuration",
   "summarize",
 ]
