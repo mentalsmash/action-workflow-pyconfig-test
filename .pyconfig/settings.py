51225502191fe44e57e525710b80c4a3fe0d1d5b
Andrea Sorbini <asorbini@rti.com>
2024-04-22 22:37:30 -0700

    Add docker build workflows

--- .pyconfig/settings.py
+++ .pyconfig/settings.py
@@ -13,13 +13,87 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 ###############################################################################
+import json
 from pathlib import Path
 from typing import NamedTuple
 
-def settings(
-    clone_dir: Path,
-    cfg: NamedTuple,
-    github: NamedTuple) -> dict:
+from pyconfig import extract_registries, tuple_to_dict, merge_dicts
+
+###############################################################################
+#
+###############################################################################
+def settings(clone_dir: Path, cfg: NamedTuple, github: NamedTuple) -> dict:
+  repo_org, repo = github.repository.split("/")
+
+  #############################################################################
+  # Debian packaging settings
+  #############################################################################
+  debian_builder_base_images_matrix = json.dumps(cfg.debian.builder.base_images)
+  debian_builder_docker_build_platforms = ",".join(
+    [f"linux/{arch}" for arch in cfg.debian.builder.architectures]
+  )
+  debian_builder_registries = extract_registries(
+    repo_org,
+    [
+      cfg.debian.builder.repo,
+      *cfg.debian.builder.base_images,
+    ],
+  )
+  debian_builder_architectures_matrix = json.dumps(cfg.debian.builder.architectures)
+
+  #############################################################################
+  # CI infrastructure settings
+  #############################################################################
+  admin_repo = cfg.ci.images.admin.image.split(":")[0]
+  admin_tag = cfg.ci.images.admin.image.split(":")[-1]
+  admin_registries = extract_registries(
+    repo_org,
+    [
+      cfg.ci.images.admin.image,
+      cfg.ci.images.admin.base_image,
+    ],
+  )
+  admin_build_platforms_config = ",".join(cfg.ci.images.admin.build_platforms)
+
+  #############################################################################
+  # Output generated settings
+  #############################################################################
   return {
-    "bar": f"{cfg.foo}.bar",
+    ###########################################################################
+    # CI config
+    ###########################################################################
+    "ci": merge_dicts(
+      {
+        "images": {
+          "admin": {
+            "login": {
+              "dockerhub": "dockerhub" in admin_registries,
+              "github": "github" in admin_registries,
+            },
+            "repo": admin_repo,
+            "tag": admin_tag,
+            "tags_config": admin_tag,
+            "build_platforms_config": admin_build_platforms_config,
+          },
+        },
+      },
+      tuple_to_dict(cfg.ci),
+    ),
+    ###########################################################################
+    # Debian config
+    ###########################################################################
+    "debian": merge_dicts(
+      {
+        "builder": {
+          "base_images_matrix": debian_builder_base_images_matrix,
+          "architectures_matrix": debian_builder_architectures_matrix,
+          "build_platforms_config": debian_builder_docker_build_platforms,
+          "login": {
+            "dockerhub": "dockerhub" in debian_builder_registries,
+            "github": "github" in debian_builder_registries,
+          },
+        },
+      },
+      tuple_to_dict(cfg.debian),
+    ),
   }
